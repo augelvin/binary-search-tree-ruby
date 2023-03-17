e07ed6ada85cea243150b29ec342bee76e0221bc
Augelvin Theone Dermaga <augelvintd@gmail.com>
2023-03-16 19:31:53 -0700

    add Tree class

--- binary-search-tree.rb
+++ binary-search-tree.rb
@@ -2,8 +2,17 @@ class Node
     include Comparable
 
   def initialize
-    @value
+    @mid
     @left
     @right
+  end
+end
+
+class Tree
+  def initialize(array)
+    @root = build_tree(array)
+  end
+
+  def build_tree(array)
   end
 end
